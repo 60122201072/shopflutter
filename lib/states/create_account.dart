6bf7b0545c4ac02d986c6e89158834f19f7e457b
Ball <anuphan.chumnum@gmail.com>
2021-07-23 15:47:49 +0700

    update

---
+++ lib/states/create_account.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  CreateAccount({Key? key}) : super(key: key);
+
+  @override
+  _CreateAccount createState() => _CreateAccount();
+}
+
+class _CreateAccount extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       
+    );
+  }
+}
